4391ae77b6e41ed169d4e43f4cc800bc6b3ba50c
Thiago Chagas <tqchagas@gmail.com>
2018-11-03 16:02:24 -0300

    update allowed hosts

--- hoteis_microsservico/settings.py
+++ hoteis_microsservico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_#m&4lfh68jhwejvrg2r^9$&tir%of!^q*s9u0xecemaykb4pl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hoteis-microsservico.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
